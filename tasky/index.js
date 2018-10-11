534ec8b7ff7a34181b197995f1af8f0af54937dd
manishsaraan <manish021js@gmail.com>
2018-10-11 22:59:13 +0530

    bounds set for  main window

--- tasky/index.js
+++ tasky/index.js
@@ -2,7 +2,7 @@ const electron = require("electron");
 const path  = require("path");
 const { app, BrowserWindow, Tray } = electron;
 
-let mainWindow;
+let mainWindow, tray;
 
 app.on("ready", () => {
     mainWindow = new BrowserWindow({
@@ -10,10 +10,32 @@ app.on("ready", () => {
         width: 300,
         frame: false,
         resizable: false,
+        show: false,
     });
+
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
     
     const iconName = process.platform === "win32" ? "windows-icon.png" : "iconTemplate@2x.png";
     const iconPath = path.join(__dirname + `/src/assets/${iconName}`);
-    new Tray(iconPath);
+    tray = new Tray(iconPath);
+    tray.on("click", (event, bounds) => {
+        // click enent bounds
+        const { x, y } = bounds;
+
+        // window height and width
+        const { height, width } = mainWindow.getBounds();
+
+        console.log(bounds.x, bounds.y);
+        if(mainWindow.isVisible()){
+            mainWindow.hide();
+        }else{
+            mainWindow.setBounds({
+                x: x - width / 2,
+                y,
+                height,
+                width,
+            })
+            mainWindow.show();
+        } 
+    })
 });
