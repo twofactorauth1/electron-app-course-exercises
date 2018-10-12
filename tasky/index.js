de55e88a6bbfd8a280aceb0d66ef0747a0bc80b6
manishsaraan <manish021js@gmail.com>
2018-10-12 22:30:28 +0530

    task icon added

--- tasky/index.js
+++ tasky/index.js
@@ -1,5 +1,6 @@
 const electron = require("electron");
-const { app, BrowserWindow } = electron;
+const path  = require("path");
+const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
 
@@ -11,4 +12,8 @@ app.on("ready", () => {
         resizable: false,
     });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+    
+    const iconName = process.platform === "win32" ? "windows-icon.png" : "iconTemplate@2x.png";
+    const iconPath = path.join(__dirname + `/src/assets/${iconName}`);
+    new Tray(iconPath);
 });
